057c73fff06094b3a276289047ece2b7f49fb470
Scott Hardy <scott.the.hardy@gmail.com>
2013-09-28 00:41:44 -0700

    added paste model to home controller, and a watcher for it

--- app/js/src/app/controllers/home_ctrl.js
+++ app/js/src/app/controllers/home_ctrl.js
@@ -1,3 +1,13 @@
 app.controller('HomeCtrl', ['$scope', function($scope) {
-  $scope.hi = 'hiiiiiiiiii';
+  $scope.paste = '';
+
+  $scope.$watch('paste', function(paste) {
+    if (paste.length == 0) {
+      console.log('naw dude');
+      return;
+    }
+    else {
+      console.log(paste);
+    }
+  });
 }]);
